06dc90cddc0e33e475475e1951ac5368948aebaf
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2023-02-22 23:41:50 +0200

    LAP3.6
    Easy

---
+++ LAP3.6.js
@@ -0,0 +1,17 @@
+function user(input){
+
+    let a = Number (input[0]);
+    if (a >= -100 && a <= 100 && a != 0){
+        console.log("Yes");
+    } else{
+        console.log("No");
+    }
+}
+
+
+
+
+
+user (["-25"])
+user (["0"])
+user (["25"])
